0e3fddd125b17a499be4792d5fc20c77b8ca0e87
alessio.marchi <alessio.marchi@accenture.com>
2022-12-07 16:33:35 +0100

    feat: add service worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV === 'development',
   dest: 'public',
+  sw: 'service-worker.gen.js',
 })
 
 /** @type {import('next').NextConfig} */
