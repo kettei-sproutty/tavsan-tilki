9b77f4492517758d9850aa2d39ec3475a41604dc
alessio.marchi <alessio.marchi@accenture.com>
2022-12-07 17:57:58 +0100

    fix: remove copyright from footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,10 +8,7 @@ export type FooterProps = {}
 const Footer: FC<FooterProps> = () => {
   return (
     <footer className='footer items-center p-4 bg-base-300 absolute bottom-0'>
-      <div className='items-center grid-flow-col'>
-        <p>Copyright © 2022 - All right reserved</p>
-      </div>
-      <div className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
+      <div className='grid-flow-col gap-4 place-self-center justify-self-end'>
         <a
           className='btn btn-ghost btn-circle'
           target={'_blank'}
