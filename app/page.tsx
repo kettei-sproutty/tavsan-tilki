32b379f2692f7f096d0a3cdd125672eb952b1874
alessio.marchi <alessio.marchi@accenture.com>
2022-12-07 17:58:34 +0100

    feat: push blue class for a11y testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 import type { FC } from 'react'
 
 const Page: FC = () => {
-  return <div />
+  return (
+    <h1 className='text-primary font-bold text-xl'>
+      Tavsan is noob in balloon game :(
+    </h1>
+  )
 }
 
 export default Page
