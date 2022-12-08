e6ce8b67a036bfc6881bcdd28381f5a264123e0a
alessio.marchi <alessio.marchi@accenture.com>
2022-12-08 18:51:31 +0100

    feat: add new icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -6,3 +6,12 @@ export type { SocialIconProps } from './Social'
 
 export { default as ThemeIcon } from './Theme'
 export type { ThemeIconProps } from './Theme'
+
+export { default as HamburgerIcon } from './Hamburger'
+export type { HamburgerIconProps } from './Hamburger'
+
+export { default as BunnyIcon } from './Bunny'
+export type { BunnyIconProps } from './Bunny'
+
+export { default as FoxIcon } from './Fox'
+export type { FoxIconProps } from './Fox'
